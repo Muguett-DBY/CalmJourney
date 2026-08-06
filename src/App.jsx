7d81887ea83686ca1167e2ab0092e75a3aed6796
Muguett-DBY <85372096+Muguett-DBY@users.noreply.github.com>
2026-08-07 05:35:34 +1000

    feat: add sensory refuge map page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage.jsx'
+import RefugePage from './pages/RefugePage.jsx'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/refuges" element={<RefugePage />} />
     </Routes>
   )
 }
